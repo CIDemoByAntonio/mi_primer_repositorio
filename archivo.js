9de51b3708020993c30d88ba5da047fd7a177978
Michael <michaelromevi@gmail.com>
2024-04-23 23:26:12 +0200

    mi primer commit

---
+++ archivo.js
@@ -0,0 +1,7 @@
+let nombre = 'Felipe'
+let apellido = ' Ramires'
+
+let nombre_completo = nombre + apellido
+
+document.write(nombre_completo.toUpperCase())  
+document.write(nombre_completo.toLowerCase())
